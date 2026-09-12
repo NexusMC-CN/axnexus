1a4caeeeef34b91cf9c02fd4bc0737e6b3b179c7
virgil698 <xiaokong23357@163.com>
2026-09-12 11:25:33 +0800

    test: import built client entrypoint

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,6 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import { createHttpClient } from '../src/index.ts';
+import { createHttpClient } from '../dist/index.js';
 
 test('exports createHttpClient', () => {
   assert.equal(typeof createHttpClient, 'function');
